c7be5c36adfd72be3b43715c7200a1b64f19ccc6
Geraldine-Okeke <me.ejiofor@unizik.edu.ng>
2023-08-26 09:49:31 +0100

    adjusted height

--- src/components/Home/Section5.js
+++ src/components/Home/Section5.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 export default function Section5(props){
   return(
     <>
-     <div className='yellow  mt-0 flex flex-row relative h-64 lg:h-72 sm:pl-10 '>
+     <div className='yellow  mt-0 flex flex-row relative h-80 sm:pl-10 '>
       <div  className="w-3/4 mt-4 pl-3">
         <Link to={props.link}>
           <button className="blue mt-10 mb-5 hover:bg-blue-700 text-black font-bold py-2 px-4 rounded-full ">
